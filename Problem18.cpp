beb1988161034b6a4faf6b6aee23b43b64c04f66
Crazy-Ginger <mattydwardle@gmail.com>
2018-12-23 22:28:40 +0000

    Proceeding with problem 18 and trying to clean up the text files

--- Problem18.cpp
+++ Problem18.cpp
@@ -2,29 +2,50 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include <sstream>
 using namespace std;
 
-int main ()
-	{
-	int row = 0, column = 0;
-	vector <vector <int>> triangle(row);
-	ifstream 	reader;
-	if (!reader)
-		{
-		cout << "Error opening trianlges" << endl;
-		return -1;
-		}
-	int linesize = 1;
-	string temp;
-	while (!reader.eof())
-		{
-		
-		triangle
-		linesize ++;
-		}
-	
-	
+int main()
+{
 	char release;
-	cin >> release;
-	return 0;
+	vector<vector<int>> triangle(0);
+	vector<int> rows(0);
+	fstream file;
+
+	file.open("Euler_Files/Probkem18_triangle.txt");
+	triangle.clear();
+
+	string str_line, str_numb;
+	int int_temp;
+	if (file.is_open())
+	{
+		while (getline(file, str_line))
+		{
+			for (char c : str_line)
+			{
+				if (c == ',' || c == '.')
+				{
+					istringstream convert(str_numb);
+
+					if (!(convert >> int_temp))
+						cout << "Error in converstion\n";
+
+					rows.push_back(int_temp);
+					str_numb.clear();
+				}
+				else
+				{
+					str_numb += c;
+				}
+			}
+			triangle.push_back(rows);
+			cout << "Converted a line and added it" << endl;
+			rows.clear();
+			str_line.clear();
+			str_numb.clear();
+		}
+
+		cin >> release;
+		return 0;
 	}
+}
