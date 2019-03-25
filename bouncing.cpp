828986846de6f2ce8c28e862e3b771810292425d
Matthew Wardle <mattydwardle@gmail.com>
2019-03-25 21:14:52 +0000

    Trying to make bouncing work but confused

--- bouncing.cpp
+++ bouncing.cpp
@@ -8,101 +8,108 @@ using namespace std;
 
 class particle
 {
-	private:
-		int currentx = 1, currenty = 1;
-		int prevx, prevy;
-		int gridw, gridh;
+    private:
+        int currentx = 1, currenty = 1;
+        int prevx, prevy;
+        int gridw, gridh;
+        int velx = 1, vely = 0;
+        
+        void boundCheck()
+        {
+            if (prevx == 0 || prevx == gridw)
+            {
+                velx = -(velx);
+            }
+            if (prevy == 0 || prevy == gridh)
+            {
+                vely = -(vely);
+            }
+        }
 
-		bool boundCheck()
-		{
+    public:
+        void setGrid(int x, int y)
+        {
+            gridw = x;
+            gridh = y;
+            currentx = (x/2);
+            currenty = (y/2);
 
-		}
-	public:
-		void setGrid(int x, int y)
-		{
-			gridw = x;
-			gridh = y;
-			currentx = (x/2);
-			currenty = (y/2);
-
-		}
+        }
+        
 };
 
 class table
 {
-  private:
-	vector<vector<char>> grid;
-	int dimx;
-	int dimy;
+    private:
+        vector<vector<char>> grid;
+        int dimx;
+        int dimy;
 
-	void set_grid()
-	{
+        void set_grid()
+        {
+            vector <char> row;
+            for (int y = 0; y <= dimy; y++)
+            {
+                for (int x = 0; x <= dimx; x++)
+                {
+                    if (y == 0 || y == dimy)
+                    {
+                        row.push_back('-');
+                    }
+                    else if (x == 0 || x == dimx)
+                    {
+                        row.push_back('|');
+                    }
+                    else
+                    {
+                        row.push_back(static_cast<char>(' '));
+                    }
 
-		vector <char> row;
-		for (int y = 0; y <= dimy; y++)
-		{
-			for (int x = 0; x <= dimx; x++)
-			{
-				if (y == 0 || y == dimy)
-				{
-					row.push_back('-');
-				}
-				else if (x == 0 || x == dimx)
-				{
-					row.push_back('|');
-				}
-				else
-				{
-					row.push_back(static_cast<char>(' '));
-				}
+                    //cout << static_cast<char>(x) << ", ";
+                }
+                grid.push_back(row);
+                row.clear();
+            }
+        }
 
-				//cout << static_cast<char>(x) << ", ";
-			}
-			grid.push_back(row);
-			row.clear();
-		}
-	}
+    public:
+        void display()
+        {
+            for (int y = 0; y <= dimy; y++)
+            {
+                cout << y << " ";
+                for (int x = 0; x <= dimx; x++)
+                {
+                    cout << grid.at(y).at(x) << " ";
+                }
+                cout << endl;
+            }
+            cout << endl;
+        }
 
-  public:
-	void display()
-	{
-		for (int y = 0; y <= dimy; y++)
-		{
-			cout << y << " ";
-			for (int x = 0; x <= dimx; x++)
-			{
-				cout << grid.at(y).at(x) << " ";
-			}
-			cout << endl;
-		}
-		cout << endl;
-	}
-
-	void set_dim(int x = 10, int y = 10)
-	{
-		dimx = x;
-		dimy = y;
-		set_grid();
-	}
+        void set_dim(int x = 10, int y = 10)
+        {
+            dimx = x;
+            dimy = y;
+            set_grid();
+        }
 };
 
 
 int main()
 {
-	table grid;
-	particle bouncer;
-	int x , y;
+    table grid;
+    particle bouncer;
+    int x , y;
 
-	cout << "Width: ";
-	cin >> x;
-	cout << "Height: ";
-	cin >> y;
+    cout << "Width: ";
+    cin >> x;
+    cout << "Height: ";
+    cin >> y;
 
-	
-	
- 	grid.set_dim(x, y);
-	bouncer.setGrid(x, y);
-	grid.display();
+    grid.set_dim(x, y);
+    bouncer.setGrid(x, y);
+    grid.display();
 
-	return 0;
+    return 0;
 }
