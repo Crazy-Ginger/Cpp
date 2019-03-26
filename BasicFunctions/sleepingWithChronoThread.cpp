c39949e3ca70d33016cd2954fdf64f47196809ec
Matthew Wardle <mattydwardle@gmail.com>
2019-03-26 17:26:58 +0000

    Created file to allow reference for idling a program across multiple platforms

---
+++ BasicFunctions/sleepingWithChronoThread.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+//for sleeping
+#include <chrono>
+#include <thread>
+using namespace std;
+
+int main()
+{
+    for (int i = 10; i >= 0; i--)
+    {
+        if (i == 0)
+        {
+            cout << "Lift off\n";
+        }
+        else if (i == 2)
+        {
+            cout << i << ": engine ignition\n";
+        }
+        else
+        {
+            cout << i << endl;
+        }
+        //seconds can be replaced with another time specifier
+        this_thread::sleep_for(chrono::seconds(1));
+    }
+    return 0;
+}
