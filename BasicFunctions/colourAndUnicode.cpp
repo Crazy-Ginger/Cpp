d53730be8adfb6a2711fb2bb9d98fd90cca30df7
Matthew Wardle <mattydwardle@gmail.com>
2019-05-20 12:35:25 +0100

    added blocks to colour test

--- BasicFunctions/colourAndUnicode.cpp
+++ BasicFunctions/colourAndUnicode.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main()
 {
-    string b = "\u03A3";
+    string block = "\u2588";
     cout << "\033[1;31mBold red text\033[0m\n";
-    cout << "\033[1;31mThis should be " << b << "\033[0m\n";
+    cout << "\033[1;31mThis should be " << block << "\033[0m\n";
     return 1;
 }
