2a89b2ee8bdebf86aec17fc85e1e89b12d8d814a
Matthew Wardle <mattydwardle@gmail.com>
2019-05-20 12:31:27 +0100

    added uniCodeTest

---
+++ unicodeTest.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    //char b = '\U00002588';
+    for (int i = 0; i < 20; i++)
+    {
+        cout << "\u2588";
+    }
+    cout << endl;
+    return 1;
+}
