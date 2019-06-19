622707eef7e7e50ea86863d535d9dbcbaf27dfc7
Rebecca Wardle <mattydwardle@gmail.com>
2019-06-19 18:10:14 +0100

    renamed reversing to stringHandling as more appropriate

---
+++ BasicFunctions/stringHandling/stringReverser.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+	{
+	string toRev;
+	getline(cin, toRev);
+	
+	string Rev = "";
+	for (int i = toRev.size(); i >= 0; i--)
+		{
+		Rev += toRev.substr(i,1);
+		}
+	cout << toRev << endl;
+	cout << Rev << endl;
+	char release;
+	cin >> release;
+	return 0;
+	}
