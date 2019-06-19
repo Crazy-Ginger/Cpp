c35c50d0a8283fbc7751cf9ff2d9106a2d2d7b10
Rebecca Wardle <mattydwardle@gmail.com>
2019-06-19 18:21:19 +0100

    made something to create duplicate file in lowercase

--- BasicFunctions/stringHandling/toLower.cpp
+++ BasicFunctions/stringHandling/toLower.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
+    vector <string> words;
+    ifstream origList;
+    origList.open("../../wordList.txt");
+    if (!origList)
+    {
+        cerr << "Unable to open list\n";
+    }
+    string parser;
+    while (origList >> parser)
+    {
+        transform(parser.begin(), parser.end(), parser.begin(), ::tolower);
+        words.push_back(parser);
+
+        parser.clear();
+    }
+    origList.close();
     
+    ofstream newList ("../../wordListLower.txt");
+    for (unsigned int i = 0; i < words.size(); i++)
+    {
+        cout << words.at(i) << endl;
+        newList << words.at(i) << endl;
+    }
+    newList.close();
     return 0;
 }
