8c171ce80dcdcfe8ab1b25b3591634a8dee57175
Rebecca Wardle <mattydwardle@gmail.com>
2019-07-04 04:02:49 +0100

    Working on prob33

--- problem33.cpp
+++ problem33.cpp
@@ -6,34 +6,47 @@ using namespace std;
 vector <int> numers, denoms;
 int main()
 {
+    bool print = false;
     for (float numerator = 10; numerator < 100; numerator++)
     {
         for (float denominator = 10; denominator < 100; denominator++)
         {
-            if (!(denominator==numerator))
-            {
-                for (int i = 0; i < 2; i++)
-                {
-                    for (int j = 0; j < 2; j++)
-                    {
-                                
-                        const string& strNum = to_string(numerator);
-                        const string& strDen = to_string(denominator);
+            cout << "num: " << numerator << "\tden: " << denominator << endl;
+            //if (numerator==49 && denominator==98)
+            //{
+                //print = true;
+                //cout << numerator << "/" << denominator << ":\n";
+            //}
+            //else
+            //{
+                //print = false;
+                //cout << "done\n";
+            //}
+
+            //if (!(denominator==numerator))
+            //{
+                //for (int i = 0; i < 2; i++)
+                //{
+                    //for (int j = 0; j < 2; j++)
+                    //{
+                        //string strNum = to_string(numerator);
+                        //string strDen = to_string(denominator);
                        
-                        if (strNum.at(i) ==strDen.at(j))
-                        {
-                            const float fltNum = stof(to_string(strNum.at(i)));
-                            const float fltDenom = stof(to_string(strDen.at(j)));
-                            if (fltNum/fltDenom == numerator/denominator)
-                            {
-                                numers.push_back(numerator);
-                                denoms.push_back(denominator);
-                                cout << numerator << "/" << denominator << endl;
-                            }
-                        }
-                    }
-                }
-            }
+                        //if (strNum.at(i) ==strDen.at(j))
+                        //{
+                            //const float fltNum = stof(to_string(strNum.at(i)));
+                            //const float fltDenom = stof(to_string(strDen.at(j)));
+                            //cout << fltNum << "/" << fltDenom << endl;
+                            //if (fltNum/fltDenom == numerator/denominator)
+                            //{
+                                //numers.push_back(numerator);
+                                //denoms.push_back(denominator);
+                                //cout << numerator << "/" << denominator << "\tadding" << endl;
+                            //}
+                        //}
+                    //}
+                //}
+            //}
         }
     }
 
