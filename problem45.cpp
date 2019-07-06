cf24f1d725fb5ab177d402d779c92bed1c7c969c
Matthew Wardle <mattydwardle@gmail.com>
2019-07-06 23:00:55 +0000

    Working on prob45

---
+++ problem45.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int tStart = 285;
+    int pStart = 165;
+    int hStart = 143;
+
+    while (tStart < 10000)
+    {
+        
+}
