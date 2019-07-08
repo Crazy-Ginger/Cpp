42a06ff1f248184c4d22eb7c7b06c5297ee137af
Matthew Wardle <mattydwardle@gmail.com>
2019-07-08 19:17:41 +0000

    work on prob45

--- problem45.cpp
+++ problem45.cpp
@@ -4,11 +4,32 @@ using namespace std;
 
 int main()
 {
-    int tStart = 285;
-    int pStart = 165;
-    int hStart = 143;
-
-    while (tStart < 10000)
+    unsigned int nT = 1; //285;
+    unsigned int nP = 1; //165;
+    unsigned int nH = 1; //143;
+    
+    bool answer = false;
+    while (!answer)
     {
-        
+       unsigned int currentT =  nT*(nT+1)/2;
+       unsigned int currentP = nP*(3*nP -1)/2;
+       while (currentT <= currentP)
+       {
+           if (currentT == currentP)
+           {
+               cout << currentT << "\tnT: " << nT << "\tnP: " << nP << endl;
+               unsigned int currentH = ;
+               while (currentT < currentH)
+               {
+
+               }
+           }
+           else
+           {
+               nP++;
+               currentP = nP*(3*nP -1)/2;
+           }
+        nT++;
+       }
+    }
 }
