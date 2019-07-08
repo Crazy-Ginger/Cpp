96ed7a945858aedaf5ee5393494734691065d543
Matthew Wardle <mattydwardle@gmail.com>
2019-07-08 00:27:52 +0000

    solved prob43

--- test.cpp
+++ test.cpp
@@ -3,89 +3,24 @@
 #include <vector>
 using namespace std;
 
-unsigned int fact(int numb)
+bool checker(string word)
 {
-    if (numb >1)
+    int primes[] = {2,3,5,7,11,13,17};
+    for (unsigned int i = 1; (i+2) < word.length(); i++)
     {
-        return numb * fact(numb-1);
+        unsigned int current = stoi(word.substr(i, 3));
+        if (current % primes[i-1] !=0)
+        { 
+            return false;
+        }
     }
-    else
-    {
-        return 1;
-    }
-}
-void printer(string word, vector<int> pointers)
-{
-    string newWord = "";
-    for (vector<int>::iterator i = pointers.begin(); i != pointers.end(); i++)
-    {
-        newWord.append(word.substr(*i, 1));
-    }
-    cout << newWord << endl;
+    cout << "Passed checkFor\n";
+    return true;
 }
 
 int main()
 {
-    string word;
-    getline(cin, word);
-    vector <int> pointers;
-    unsigned int length = word.size();
-    
-    for (unsigned int i = 0; i < length; i++)
-    {
-        pointers.push_back(i);
-        cout << i;
-    }
-    cout << endl;
-
-    vector<int>::iterator initial_comp, rearrange, asc_swapper;
-    unsigned int count = 1;
-    cout << "Count: " << count << "\t";
-    printer(word, pointers);
-    count++;
-    while (count <= fact(length))
-    {
-        initial_comp = pointers.end()-2;
-        while (*initial_comp > 0)
-        {
-            if (*initial_comp < *(initial_comp+1))
-            {
-                break;
-            }
-            else
-            {
-                initial_comp--;
-            }
-        }
-        rearrange = initial_comp + 1;
-        asc_swapper = pointers.end();
-            
-        //arranges all the numbers after initial_comp into ascending order
-        while (rearrange < asc_swapper)
-        {
-            unsigned int swapper = *rearrange;
-            *rearrange = *asc_swapper;
-            *asc_swapper = swapper;
-            rearrange++;
-            asc_swapper--;
-        }
-        rearrange = pointers.end();
-       
-        //finds the the largest number at the back so that it can be swapped
-        while (*rearrange > *initial_comp)
-        {
-            rearrange--;
-        }
-    
-        //swaps the largest number at the back of pointers with the number just infront of it
-        rearrange++;
-        unsigned int swapper = *initial_comp;
-        *initial_comp = *rearrange;
-        *rearrange = swapper;
-
-        cout << "Count: " << count << "\t";
-        printer(word, pointers);
-        count++;
-    }
+    string word = "1406357289";
+    cout << checker(word) << endl;
     return 0;
 }
