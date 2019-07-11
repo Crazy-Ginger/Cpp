bfe50572e526fa8f5b6479df5ba93e6685eca4f0
Rebecca Wardle <mattydwardle@gmail.com>
2019-07-11 02:27:53 +0100

    work on test for prob49

--- test.cpp
+++ test.cpp
@@ -5,6 +5,10 @@ using namespace std;
 int main()
 {
     vector <int> list = {1487, 1847, 4817, 8147};
-    
+    int diff;
+    for (unsigned int i = 0; i < list.size()-1; i++)
+    {
+
     return 0;
 }
+
