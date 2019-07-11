f5e95f7e748e75dcde3975ed963a710208d218fd
Matthew Wardle <mattydwardle@gmail.com>
2019-07-11 03:07:42 +0000

    solved prob49

--- test.cpp
+++ test.cpp
@@ -5,9 +5,22 @@ using namespace std;
 int main()
 {
     vector <int> list = {1487, 1847, 4817, 8147};
-    int diff;
+    int diff, lastDiff = 0;
     for (unsigned int i = 0; i < list.size()-1; i++)
     {
+        lastDiff = 0;
+        cout << "i: " << i  << endl;
+        for (unsigned int j = i+1; j < list.size(); j++)
+        {
+            diff = list.at(j) - list.at(i);
+            if (diff == lastDiff*2)
+            {
+                cout << "at i: " << list.at(i) << "\t" << list.at(j-1) << "\t" << list.at(j) << endl;
+            }
+            cout << diff << endl;
+            lastDiff = diff;
+        }
+    }
 
     return 0;
 }
