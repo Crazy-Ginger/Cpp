6c41d2f2dad33a5a5f94d8086a95dcbc561dacd7
Matthew Wardle <mattydwardle@gmail.com>
2019-07-24 12:22:29 +0000

    added options for movellipse so they can be in co, grey or cannied

--- opencv/MovingEllipses/ellipse.cpp
+++ opencv/MovingEllipses/ellipse.cpp
@@ -3,6 +3,9 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 #include <vector>
+#include <string> //for userinput
+#include <algorithm> //converting user input to lowercase
+//for the random generation of the ellipses
 #include <time.h>
 #include <random>
 using namespace std;
@@ -35,8 +38,20 @@ int main(int argc, char* argv[])
     //establishes the random stuff to allow randomisation of the start angle and colour of the ellipses
     random_device rd;
     default_random_engine rng(rd());
-    uniform_int_distribution <> angleRange(5, 180);
-    uniform_int_distribution <> colourRange(0, 255);
+    uniform_int_distribution <> aRand(5, 180);
+    uniform_int_distribution <> coRand(0, 255);
+    
+    string choice = "";
+    if (argc > 1)
+    {
+        choice = argv[2];
+    }
+    else
+    {
+        cout << "Error inputs required\n";
+        return 0;
+    }
+    transform(choice.begin(), choice.end(), choice.begin(), ::tolower);
 
     //takes the user input size for the image
     int canSize = atoi(argv[1]);
@@ -48,22 +63,24 @@ int main(int argc, char* argv[])
     {
         movLipse newOne;
         newOne.thickness = canSize/100;
-        newOne.angle = angleRange(rng);
+        newOne.angle = aRand(rng);
         newOne.setCentre(canSize/2, canSize/2);
         newOne.height = canSize/2 -20;
         newOne.width = canSize/8 -20;
-        newOne.colour = Scalar(255, 255, 255);
+        if (choice != "cannied" || choice != "ca"){
+            newOne.colour = Scalar(coRand(rng), coRand(rng), coRand(rng));
+        }
+        else{
+            newOne.colour = Scalar(255, 255, 255);
+        }
         lipses.push_back(newOne);
     }
     
     
     for (double i = 0; i < 360; i++)
     {
-        Point seed(3,3);
         //creates the basic frame that will contain the basic ellipes
-        Mat frame = Mat::zeros(canSize, canSize, CV_8UC3);
-        //initiallises the other mats to hold the greyscale, cannied and filled ellipses
-        Mat greyFrame, cframe, mask, fillFrame;
+        Mat coFrame = Mat::zeros(canSize, canSize, CV_8UC3);
 
         //sets the name of the images to be shown and establishes the window and resets its position
         char windowName[] = "frame";
@@ -74,54 +91,42 @@ int main(int argc, char* argv[])
         for (unsigned int j = 0; j < lipses.size(); j++)
         {
             //adds the ellipse to the image
-            lipses.at(j).addtoImg(frame);
+            lipses.at(j).addtoImg(coFrame);
             //adds to the angle for its next display time
             lipses.at(j).angle += j/2 +1;
         }
-        //greyscales and then blurs the entire image (to be cannied) and saves it over the grey image mat
-        cvtColor(frame, greyFrame, COLOR_BGR2GRAY);
-        blur(greyFrame, greyFrame, Size(2,2));
-        //cannies the entire image and saves it as a new image mat
-        Canny(greyFrame, cframe, 10, 30, 5);
-        
-        /*{
-        //tryied to create code that would fill the inside of the cannied ellipses but has problems with the edges being incomplete
-        //copies the cannied image onto mask and then...?
-        mask = cframe;
-        fillFrame = cframe;
-        copyMakeBorder(mask, mask, 1, 1, 1, 1, BORDER_REPLICATE);
-
-        //creating a bunch of throwaway variables and gets a point that should be inside the ellipses
-
-        int ellipseEdge = 0;
-        bool passedWhite = false, cont = false;
-        while (!cont)
+        if (choice == "colour" || choice == "co")
         {
-            if ((fillFrame.at<int>(canSize/2, ellipseEdge) == 0) && passedWhite)
+            imshow(windowName, coFrame);
+            waitKey(40);
+        }
+        else
+        {
+            //initiallises the other mats to hold the greyscale, cannied and filled ellipses
+            Mat gFrame, caFrame;
+
+            //greyscales and then blurs the entire image (to be cannied) and saves it over the grey image mat
+            cvtColor(coFrame, gFrame, COLOR_BGR2GRAY);
+            
+            if (choice == "grey" || choice == "g")
             {
-                cont = true;
+                imshow(windowName, gFrame);
+                waitKey(40);
             }
-            else if (fillFrame.at<int>(canSize/2, ellipseEdge) !=0)
+            else if (choice == "cannied" || choice == "ca")
             {
-                passedWhite = true;
-                ellipseEdge ++;
+                blur(gFrame, gFrame, Size(2,2));
+                //cannies the entire image and saves it as a new image mat
+                Canny(gFrame, caFrame, 10, 30, 5);
+                
+                imshow(windowName, caFrame);
+                waitKey(40);
             }
-            else 
+            else
             {
-                ellipseEdge++;
+                cout << "invalid input\n";
             }
         }
-        Rect recta;
-        //fills the spaces with white block
-        floodFill(fillFrame, mask, Point(canSize/2, ellipseEdge), Scalar(100), &recta, Scalar(0), Scalar(230));
-
-        Mat colourFill;
-        cvtColor(fillFrame, colourFill, COLOR_GRAY2BGR);
-        circle(colourFill, Point(canSize/2, ellipseEdge), 5, Scalar(0,0,255),FILLED, LINE_8);
-        //displays the image and then waits 50 milliseconds before destroing it and running again
-        imshow(windowName, colourFill);}*/
-        imshow(windowName, cframe);
-        waitKey(40);
     }
     return 0;
 }
