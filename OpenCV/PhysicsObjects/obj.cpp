d5a15c1d2a10d34d5f791825f36f734ab6f5f915
Matthew Wardle <mattydwardle@gmail.com>
2019-07-29 02:24:45 +0000

    modified physics objs so ball now have random start and direction, also most funcitons are defined in class

--- OpenCV/PhysicsObjects/obj.cpp
+++ OpenCV/PhysicsObjects/obj.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 
+#include <random>
+#include <time.h>
+
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
@@ -8,43 +11,116 @@
 using namespace std;
 using namespace cv;
 
+int width, height;
+
 class ball
 {
     public:
+        //constructor called when new object is instantiated
+        ball()
+        {
+            random_device rd;
+            default_random_engine rng(rd());
+            uniform_int_distribution <> wCoord(width/5, 4*(width/5));
+            uniform_int_distribution <> hCoord(height/5, 4*(height/5));
+            setCen(wCoord(rng), hCoord(rng));
+
+            //should generate a random direction for the ball
+            uniform_int_distribution <> boolGen(0, 1);
+            if (boolGen(rng) == 0)
+            {
+                down = true;
+            }
+            else down = false;
+
+            if (boolGen(rng) == 0)
+            {
+                right = true;
+            }
+            else right = false;
+        }
         Point centre;
         Scalar colour;
         int rad;
         int xcoord, ycoord;
+        bool right , down;
 
-    void setCen(int x, int y)
-    {
-        centre = Point(x,y);
-        xcoord = x;
-        ycoord = y;
-    }
-    void setColour(int r, int g, int b)
-    {
-        colour = Scalar(r, g, b);
-    }
-    void addtoImg(Mat img)
-    {
-        circle(img, centre, rad, colour, FILLED, LINE_8);
-    }
+        //sets the centre of the object and assigns it to the variables
+        void setCen(int x, int y)
+        {
+            centre = Point(x,y);
+            xcoord = x;
+            ycoord = y;
+        }
+        //sets the colour of the circle
+        void setColour(int r, int g, int b)
+        {
+            colour = Scalar(r, g, b);
+        }
+        //adds the object to the image
+        void addtoImg(Mat img)
+        {
+            circle(img, centre, rad, colour, FILLED, LINE_8);
+        }
+        //checks the object isn't in a wall
+        void wallCol()
+        {
+            if (rad + xcoord > width)
+            {
+                right = false;
+            }
+            if (rad + ycoord > height)
+            {
+                down = false;
+            }
+            if (xcoord - rad < 0)
+            {
+                right = true;
+            }
+            if (ycoord - rad < 0)
+            {
+                down = true;
+            }
+
+            //int x = obj.xcoord, y = obj.ycoord;
+            if (!right)
+            {
+                setCen(--xcoord, ycoord);
+            }
+            if (!down)
+            {
+                setCen(xcoord, --ycoord);
+            }
+            if (down)
+            {
+                setCen(xcoord, ++ycoord);
+            }
+            if (right)
+            {
+                setCen(++xcoord, ycoord);
+            }
+        }
 };
 
-unsigned int t = 0;
 
 int main(int argc, char* argv[])
 {   
-    int height = atoi(argv[1]), width = atoi(argv[2]), rad = atoi(argv[3]);
-    
+    //checks for the correct number of inputs
+    if (argc != 5)
+    {
+        cout << "Inputs required\n1st Window Height\n2nd Window width\n3rd Circle radius\n4th Speed\n";
+        return -1;
+    }
+
+    height = atoi(argv[1]);
+    width = atoi(argv[2]);
+    int rad = atoi(argv[3]);
+
     //initialise physics object
     ball obj;
     obj.rad = rad;
-    obj.setCen(width/2, height/2);
     obj.setColour(100, 150, 200);
 
-    bool right = true, down = true;
     //infinite loop
     for (;;)
     {
@@ -55,41 +131,8 @@ int main(int argc, char* argv[])
         moveWindow(name, 0, 0);
 
         obj.addtoImg(canvas);
-        
-        if (rad + obj.xcoord > width)
-        {
-            right = false;
-        }
-        if (rad + obj.ycoord > height)
-        {
-            down = false;
-        }
-        if (obj.xcoord - rad < 0)
-        {
-            right = true;
-        }
-        if (obj.ycoord - rad < 0)
-        {
-            down = true;
-        }
 
-        //int x = obj.xcoord, y = obj.ycoord;
-        if (!right)
-        {
-            obj.setCen(--obj.xcoord, obj.ycoord);
-        }
-        if (!down)
-        {
-            obj.setCen(obj.xcoord, --obj.ycoord);
-        }
-        if (down)
-        {
-            obj.setCen(obj.xcoord, ++obj.ycoord);
-        }
-        if (right)
-        {
-            obj.setCen(++obj.xcoord, obj.ycoord);
-        }
+        obj.wallCol();
 
         imshow(name, canvas);
         waitKey(atoi(argv[4]));
