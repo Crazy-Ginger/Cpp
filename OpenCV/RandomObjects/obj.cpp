1616315e32970a95e03f496c255ed0600042b463
Matthew Wardle <mattydwardle@gmail.com>
2019-07-31 23:02:02 +0000

    Started work on a 'random' object in cpp

---
+++ OpenCV/RandomObjects/obj.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <time.h>
+#include <random>
+#include <algorithm>
+
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char* argv[])
+{
+    random_device rd;
+    default_random_engine rng(rd());
+    uniform_int_distribution <> rColour(0, 255);
+    uniform_int_distribution <> objNum(700, 7000);
+    uniform_int_distribution <> dic();
+    
+    int dim = atoi(argv[1]);
+    Mat coFrame = Mat::zeros(dim, dim, CV_8UC3);
+    char windowName[] = "random image";
+    string choice = argv[2];
+    transform(choice.begin(), choice.end(), choice.begin(), ::tolower);
+
+    namedWindow(windowName);
+    moveWindow(windowName, 0, 0);
+
+    if (choice == "g" || choice == "gr")
+    {
+        Mat gFrame;
+    }
+    else if (choice == "ca" || choice == "cannied")
+    {
+        Mat gFrame, caFrame;
+    }
+    
+    return 0;
+}
