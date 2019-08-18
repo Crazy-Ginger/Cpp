edfb5a39c92ef3b885df18b982ff49f4f3050b8f
Matthew Wardle <mattydwardle@gmail.com>
2019-08-18 16:38:28 +0000

    trying to re-write angle itterator

--- OpenCV/Planets/planets.cpp
+++ OpenCV/Planets/planets.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
@@ -58,19 +59,24 @@ int main(int argc, char* argv[])
         newOne.height = (rad+itter)*abs(cos(ang));
         //newOne.width = rad +itter;
         //newOne.height = height + cos(3.14/ang)*itter;
-        newOne.colour = Scalar(rCol(rng)+29, gCol(rng)+27, bCol(rng)+20);
+        newOne.colour = Scalar(bCol(rng)+20, gCol(rng)+27, rCol(rng)+29);
         lipses.push_back(newOne);
         itter += 1;
     }
 
-    bool down = true;
-    int rAngle = aRand(rng), angSpeed = 3;
+    //bool down = true;
+    //angSpeed = 3;
     //creates loop to allow oscilation
     itter = 1;
+    double doubItter = 0;
+    int rAngle;
     for (;;)
     {
-        //establishes all the image stuff such as planet and move the window
+        rAngle = 20 * cos(doubItter);
+
+        //establishes all the image details such as planet and moves the window
         Mat frame = Mat::zeros(canSize, canSize*1.7, CV_8UC3);
+        //string windowName = to_string(rAngle);
         char windowName[] = "frame";
         namedWindow(windowName);
         moveWindow(windowName, 0, 0);
@@ -82,45 +88,24 @@ int main(int argc, char* argv[])
             //adds the lipses to the image
             lipses.at(i).addtoImg(frame, rAngle);
         } 
-        //checks if the lipses are at the max on min height and then if they're heading up or down and then increments or decrements the angle for the next frame
-        //if (rAngle > 20)
-        //{
-            //rAngle--;
-            //down =  true;
-        //}
-        //else if (rAngle < -20)
-        //{
-            //rAngle++;
-            //down = false;
-        //}
-        //else if (down)
-        //{
-            //rAngle--;
-        //}
-        //else if (!down)
-        //{
-            //rAngle++;
-        //}
-        //else
-        //{
-            //cout << "um... something gone wrong: " << rAngle << endl; 
-            //char release;
-            //cin >> release;
-        //}
 
-        rAngle = cos(0.09*itter) - cos(0.09*(itter+1));
-    
         //adds another elipse to the image to cover back of the ellipses
         ellipse(frame, Point((canSize*1.7)/2, canSize/2), Size(canSize/6.5, canSize/6.5), rAngle, 180, 360, Scalar(255,0,0), FILLED, LINE_8);
         
         //displays the image then waits 40 milliseconds
         imshow(windowName, frame);
-        waitKey(60);
-        if (rAngle == 0)
+        waitKey(10);
+
+        if (doubItter >= 3.14*2)
         {
-            itter = 0;
+            doubItter = 0;
+            //cout << "reset doubItter\n";
         }
-        itter++;
+        else 
+        {
+            doubItter += 0.01;
+        }
+        //cout << "doubItter: " << doubItter << endl;
     }
     return 1;
 }
--- OpenCV/TestBed/testImage.cpp
+++ OpenCV/TestBed/testImage.cpp
@@ -13,11 +13,8 @@ int main()
     moveWindow(winName, 0, 0);
 
     Mat image = Mat::zeros(700, 700, CV_8UC3);
-    circle(image, Point(350,350), 80, Scalar(255,0, 0), 1, LINE_4);
+    circle(image, Point(350,350), 80, Scalar(0,0,255), 1, LINE_4);
 
-    circle(image, Point(350,350), 82, Scalar(0,0,255), 1, LINE_8);
-    //string text = to_string(i);
-    //putText(image, text, Point (100, 100), 20, 1, Scalar(200,200,200)); 
     imshow(winName, image);
     waitKey(0);
     return 0;
