4155cb0c3dcf8563f58b58e080b1d09660c65b03
Matthew Wardle <mattydwardle@gmail.com>
2019-08-21 04:30:34 +0000

    set a checklist of 3D objects

--- OpenCV/3DObjects/objects.cpp
+++ OpenCV/3DObjects/objects.cpp
@@ -6,4 +6,12 @@
 using namespace std;
 using namespace cv;
 
-
+//look into rendering 3D objects with purely lines and then decreasing the line darkness when displaying the line
+//
+//looking into converting an ASCII stl file into an object then using opencv to display that object to the user
+//
+//need to figure out how to store 3d objects in decent way
+//
+//need to learn how to interpret stl files
+//
+//look into efficient methods of traversing 3D arrays and matricies
