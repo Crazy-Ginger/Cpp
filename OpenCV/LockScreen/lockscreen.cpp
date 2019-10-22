f62b3a11a66024a7584103bfc984ef78a58e9fa4
Rebecca Wardle <mattydwardle@gmail.com>
2019-10-22 15:29:02 +0100

    Working on Lockscreen

--- OpenCV/LockScreen/lockscreen.cpp
+++ OpenCV/LockScreen/lockscreen.cpp
@@ -1,4 +1,47 @@
 #include <iostream>
 #include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+using namespace std;
+using namespace cv;
 
-// write python code into C++ for speed then use that in lockscreen
+
+
+int main(int argc, char* argv[])
+{
+    if (argc != 4)
+    {
+        cout << "Input's required:\n1.Pixelating block Height\n2.Pixelating block Width\n";
+    }
+
+    uint8_t blockH = stoi(argv[1]);
+    uint8_t blockW = stoi(argv[2]);
+
+    cout << argv[3] << endl;
+    
+    Mat img;
+    img = imread(argv[3], IMREAD_COLOR);
+    if (!img.data)
+    {
+        cout << "Error couldn't read the file\n";
+        return 1;
+    }
+    uint8_t height = img.rows;
+    uint8_t widths = img.cols;
+    
+    char release;
+    for(uint8_t x = 0; x < widths - blockW; x+= blockW)
+    {
+        for (uint8_t y = 0; y < height - blockH; y+= blockH)
+        {
+            Vec3b bgrPixel = img.at<Vec3b>(y, x);
+            cout << bgrPixel << endl;
+            cin >> release;
+        }
+    }
+
+    namedWindow( "Display Image", WINDOW_AUTOSIZE);
+    imshow("Display Image", img);
+    waitKey(0);
+    return 0;
+}
