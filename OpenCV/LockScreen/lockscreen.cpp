e4c3e25ffb2c19832b61045cabfa8f8a247eb001
Rebecca Wardle <mattydwardle@gmail.com>
2019-10-23 02:07:30 +0100

    Working on Lockscreen

--- OpenCV/LockScreen/lockscreen.cpp
+++ OpenCV/LockScreen/lockscreen.cpp
@@ -6,7 +6,25 @@
 using namespace std;
 using namespace cv;
 
+void setBlock()
+{
 
+}
+
+
+int getBlock(Mat img, int widths, int height, int blockW, int blockH, int channel)
+{
+    int total = 0;
+    for (int x = 0; x < blockW; x++)
+    {
+        for (int y =0;y < blockW; y++)
+        {
+            total += img.at<Vec3b>(Point(height+y, widths+x))[channel];
+        }
+    }
+    int average = total / (blockH * blockW);
+    return average;
+}
 
 int main(int argc, char* argv[])
 {
@@ -27,19 +45,25 @@ int main(int argc, char* argv[])
         cout << "Error couldn't read the file\n";
         return 1;
     }
-    //int height = img.rows;
-    //int widths = img.cols;
+    int height = img.rows;
+    int widths = img.cols;
     
+    Mat newImg = Mat(height, widths, CV_8UC3, Scalar(0,0,0));
+
     char release;
-    //for(int x = 0; x < widths - blockW; x+= blockW)
-    //{
-        //for (int y = 0; y < height - blockH; y+= blockH)
-        //{
-            ////Vec3b bgrPixel = img.at<Vec3b>(y, x);
-            ////cout << bgrPixel << endl;
-            //cin >> release;
-        //}
-    //}
+    for(int x = 0; x < widths - blockW; x+= blockW)
+    {
+        for (int y = 0; y < height - blockH; y+= blockH)
+        {
+            Vec3b bgrPixel = img.at<Vec3b>(y, x);
+            for (int k = 0; k < 3; k++)
+            {
+                setBlock(getBlock(img, x, y, blockW, blockH, k), x, y, blockW, blockH, k);
+            }
+            cout << bgrPixel << endl;
+            cin >> release;
+        }
+    }
 
     namedWindow( "Display Image", WINDOW_AUTOSIZE);
     imshow("Display Image", img);
