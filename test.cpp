78659a9bb76d5240dbf487b890322b4daa58d4d7
Rebecca Wardle <mattydwardle@gmail.com>
2020-02-20 05:30:40 +0000

    Modified test?

--- test.cpp
+++ test.cpp
@@ -1,18 +1,6 @@
 #include <iostream>
-#include <string>
-using namespace std;
 
-
-int main(int argc, char* argv[])
+int main()
 {
-    if (argv[1])
-    {
-        cout << argv[1] << endl;
-    }
-    else
-    {
-        cout << "Nothing\n";
-    }
     return 0;
 }
-
